eb2dc7b98566efb266e7a7804942828076ebdecf
Mamun-Udoy <abdullah15-2696@diu.edu.bd>
2024-01-17 13:26:06 +0600

    Network retorfit object and hilt add done

---
+++ app/src/main/java/com/example/youtube/utils/AppConstant.kt
@@ -0,0 +1,10 @@
+package com.example.youtube.utils
+
+const val BASE_URL = "https://serpapi.com"
+const val POST_LIST = "/posts"
+const val SEARCH = "/search.json"
+const val API_KEY = "8c80cb6fa1c782f0dea607c1b6f05fea276d1d4527dfc393ed3cc66d12bd707d"
+const val DEVICE = "mobile"
+const val ENGINE = "google_videos"
+const val HL = "en"
+const val GL = "us"
